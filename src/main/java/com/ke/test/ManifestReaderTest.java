01a9f881f71fca435af1d40d8ba7382234b9a846
kewangie <ke.wang@teamaol.com>
2014-07-16 05:12:42 +0100

    Manifest reader

---
+++ src/main/java/com/ke/test/ManifestReaderTest.java
@@ -0,0 +1,13 @@
+package com.ke.test;
+
+import java.io.IOException;
+
+public class ManifestReaderTest {
+
+	public static void main(String[] args) throws IOException {
+
+		new ManifestReader().readManifest();
+
+	}
+
+}
